4db6bd75e0231035a88f8edb5ad2a60077bddf70
Bianca Mendes <71153572+b-bianca@users.noreply.github.com>
2023-11-02 15:54:37 -0300

    add health check (#19)

--- internal/manage.go
+++ internal/manage.go
@@ -3,6 +3,7 @@ package manage
 import (
 	"soat1-challenge1/internal/core/ports"
 	c "soat1-challenge1/internal/handlers/customer"
+	h "soat1-challenge1/internal/handlers/health_check"
 	o "soat1-challenge1/internal/handlers/order"
 	p "soat1-challenge1/internal/handlers/product"
 	pc "soat1-challenge1/internal/handlers/product_category"
@@ -16,6 +17,7 @@ type apps interface {
 
 // Manage holds client instance
 type Manage struct {
+	hc              apps
 	order           apps
 	product         apps
 	productCategory apps
@@ -32,13 +34,14 @@ type UseCases struct {
 
 // New creates a new client to create new routes
 func New(uc *UseCases) *Manage {
-
+	healthCheckHandler := h.NewHandler()
 	orderHandler := o.NewHandler(uc.Order)
 	productHandler := p.NewHandler(uc.Product)
 	customerHandler := c.NewHandler(uc.Customer)
 	productCategoryHandler := pc.NewHandler(uc.ProductCategory)
 
 	return &Manage{
+		hc:              healthCheckHandler,
 		order:           orderHandler,
 		product:         productHandler,
 		customer:        customerHandler,
@@ -48,6 +51,7 @@ func New(uc *UseCases) *Manage {
 
 // RegisterRoutes register routes and set its permissions
 func (m *Manage) RegisterRoutes(group *gin.RouterGroup) {
+	m.hc.RegisterRoutes(group)
 	m.order.RegisterRoutes(group)
 	m.product.RegisterRoutes(group)
 	m.customer.RegisterRoutes(group)
